9e02ba9f94ea4d9fb14559b58274b2abe57c29b2
Rafael Mendes <rafadeoliveiramendes@gmail.com>
2026-05-28 01:18:26 -0300

    feat: rename component to NavbarButton, add styles and close/open logic

--- src/components/NavbarButton.tsx
+++ src/components/NavbarButton.tsx
@@ -1,11 +1,17 @@
-import { Menu } from "lucide-react";
+import { Menu } from 'lucide-react'
 
-export default function ButtonIcon() {
-  return (
-    <button
-      className=""
-    >
-      <Menu size={24} strokeWidth={2} />
-    </button>
-  );
+interface NavbarButtonProps {
+	isOpen: boolean
+	onClick: () => void
+}
+
+export default function NavbarButton(props: NavbarButtonProps) {
+	return (
+		<button
+			className={`bg-[#102026] absolute top-4 right-0 transition-transform duration-300 md:hidden rounded-full p-3 ${props.isOpen ? 'translate-x-1/2' : 'translate-x-[125%]'}`}
+			onClick={props.onClick}
+		>
+			<Menu size={24} strokeWidth={2} color='white' />
+		</button>
+	)
 }
