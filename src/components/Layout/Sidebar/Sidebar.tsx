2940db209dbce0a475016efd09ced72a84b0cb19
Rafael Mendes <rafadeoliveiramendes@gmail.com>
2026-05-28 01:18:37 -0300

    fix: correct import statement and update to use NavbarButton

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import ButtonIcon from "../../NavbarButton"
+import NavbarButton from '../../NavbarButton'
 
 interface SidebarProps {
 	isOpen: boolean
@@ -8,12 +8,16 @@ interface SidebarProps {
 export default function Sidebar(props: SidebarProps) {
 	return (
 		<>
-			{props.isOpen && <div className="fixed inset-0 bg-black/50 z-40 md:hidden" onClick={()=>props.setIsOpen(false)}></div>}
+			{props.isOpen && (
+				<div
+					className='fixed inset-0 bg-black/50 z-40 md:hidden'
+					onClick={() => props.setIsOpen(false)}
+				></div>
+			)}
 			<nav
 				className={`bg-[#12121a] fixed top-0 left-0 h-dvh w-64 z-50 transition-transform duration-300 md:relative md:translate-x-0 ${props.isOpen ? 'translate-x-0' : '-translate-x-full'}`}
 			>
-				<p className='text-white'>Sidebar</p>
-				<ButtonIcon />
+				<NavbarButton isOpen={props.isOpen} onClick={() => props.setIsOpen(!props.isOpen)} />
 			</nav>
 		</>
 	)
