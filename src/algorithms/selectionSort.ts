6de4a24f6a7dc925b2c741e5b2fea825765788b5
Rafael Mendes <rafadeoliveiramendes@gmail.com>
2026-06-10 23:13:47 -0300

    feat: add selection sort algorithm

---
+++ src/algorithms/selectionSort.ts
@@ -0,0 +1,26 @@
+export default function selectionSort(array: number[]) {
+   const sortedArray: number[] = [...array]
+
+   for (let i = 0; i < sortedArray.length; i++) {
+      const first = sortedArray[i]
+      let minIndex = i
+
+      for (let n = i + 1; n < sortedArray.length; n++) {
+
+         const target = sortedArray[n]
+
+         if (target < first) {
+            minIndex = n
+         }
+      }
+
+      if (i != minIndex) {
+         const temp = sortedArray[i]
+         sortedArray[i] = sortedArray[minIndex]
+         sortedArray[minIndex] = temp
+      }
+
+   }
+
+   return sortedArray
+}
