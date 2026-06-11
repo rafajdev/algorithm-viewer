657306aa24301b392022e11d0dfa38dc65c0acd6
Rafael Mendes <rafadeoliveiramendes@gmail.com>
2026-06-11 17:36:28 -0300

    style: format selectionSort function for consistency

--- src/algorithms/selectionSort.ts
+++ src/algorithms/selectionSort.ts
@@ -1,26 +1,24 @@
 export default function selectionSort(array: number[]) {
-   const sortedArray: number[] = [...array]
+	const sortedArray: number[] = [...array]
 
-   for (let i = 0; i < sortedArray.length; i++) {
-      const first = sortedArray[i]
-      let minIndex = i
+	for (let i = 0; i < sortedArray.length; i++) {
+		const first = sortedArray[i]
+		let minIndex = i
 
-      for (let n = i + 1; n < sortedArray.length; n++) {
+		for (let n = i + 1; n < sortedArray.length; n++) {
+			const target = sortedArray[n]
 
-         const target = sortedArray[n]
+			if (target < first) {
+				minIndex = n
+			}
+		}
 
-         if (target < first) {
-            minIndex = n
-         }
-      }
+		if (i != minIndex) {
+			const temp = sortedArray[i]
+			sortedArray[i] = sortedArray[minIndex]
+			sortedArray[minIndex] = temp
+		}
+	}
 
-      if (i != minIndex) {
-         const temp = sortedArray[i]
-         sortedArray[i] = sortedArray[minIndex]
-         sortedArray[minIndex] = temp
-      }
-
-   }
-
-   return sortedArray
+	return sortedArray
 }
