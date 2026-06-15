01ad29e5e9fc9e0ba38d4fac1edeab5b6a44b373
Rafael Mendes <rafadeoliveiramendes@gmail.com>
2026-06-15 19:50:41 -0300

    feat: changes component name

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -1,11 +1,11 @@
 import { Menu } from 'lucide-react'
 
-interface NavbarButtonProps {
+interface SidebarButtonProps {
 	isOpen: boolean
 	onClick: () => void
 }
 
-export default function NavbarButton(props: NavbarButtonProps) {
+export default function SidebarButton(props: SidebarButtonProps) {
 	return (
 		<button
 			className={`bg-[#102026] absolute top-4 right-0 transition-transform duration-300 md:hidden rounded-full p-3 ${props.isOpen ? 'translate-x-1/2' : 'translate-x-[125%]'}`}
