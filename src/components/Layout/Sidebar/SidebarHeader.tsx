995bb7c918711518ba77cb82dd7fd4a6068b6168
Rafael Mendes <rafadeoliveiramendes@gmail.com>
2026-06-16 10:26:15 -0300

    feat: adjust spacing in SidebarHeader and update heading size

--- src/components/Layout/Sidebar/SidebarHeader.tsx
+++ src/components/Layout/Sidebar/SidebarHeader.tsx
@@ -2,10 +2,10 @@ import SidebarHeaderIcon from './SidebarHeaderIcon'
 
 export default function SidebarHeader() {
 	return (
-		<header className='px-7 pt-5 flex gap-4'>
+		<header className='px-7 pt-5 flex gap-2 md:gap-4'>
 			<SidebarHeaderIcon />
 			<div className='font-["Rajdhani"] flex flex-col'>
-				<h1 className='text-white text-xl md:text-2xl font-semibold tracking-tight'>
+				<h1 className='text-white text-2xl font-semibold tracking-tight'>
 					ALGORITHM VIEWER
 				</h1>
 				<h3 className='text-[#00ffd0] font-semibold tracking-widest'>VISUALIZE HOW ALGORITHMS WORK</h3>
