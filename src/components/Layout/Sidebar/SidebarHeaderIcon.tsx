8877fa3289b162deba60f7e58538404f4219a7be
Rafael Mendes <rafadeoliveiramendes@gmail.com>
2026-06-16 10:30:31 -0300

    fix: adjust width and height for responsive design

--- src/components/Layout/Sidebar/SidebarHeaderIcon.tsx
+++ src/components/Layout/Sidebar/SidebarHeaderIcon.tsx
@@ -2,7 +2,7 @@ import { ChartColumn } from "lucide-react";
 
 export default function SidebarHeaderIcon() {
 	return (
-		<div className='bg-[#064d43] w-12 h-15 rounded-sm flex items-center justify-center'>
+		<div className='bg-[#064d43] w-10 h-13 md:w-12 md:h-15 rounded-sm flex items-center justify-center'>
 			<ChartColumn color='#00ffd0' strokeWidth={2.5}/>
 		</div>
 	)
